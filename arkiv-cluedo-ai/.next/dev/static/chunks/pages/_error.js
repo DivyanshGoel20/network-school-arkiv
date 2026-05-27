608f427ccbd24354a4aecf77541a967f520455dc
Divyansh Goel <divyanshgoel17@gmail.com>
2026-05-27 15:47:47 +0530

    fix issues

---
+++ arkiv-cluedo-ai/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/node_modules_next_dist_compiled_0o6l_m6._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_0n1wg2c._.js",
+  "static/chunks/node_modules_next_dist_client_0pe1dg-._.js",
+  "static/chunks/node_modules_next_dist_0k8zr~w._.js",
+  "static/chunks/07wo_next_dist_pages__error_0aovv4~.js",
+  "static/chunks/[next]_entry_page-loader_ts_02w7xhy._.js",
+  "static/chunks/node_modules_react-dom_0bruynb._.js",
+  "static/chunks/node_modules_0lx093h._.js",
+  "static/chunks/[root-of-the-server]__0iuona1._.js",
+  "static/chunks/[project]_network-school-arkiv_arkiv-cluedo-ai_pages__error_07xvfw~._.js",
+  "static/chunks/turbopack-[project]_network-school-arkiv_arkiv-cluedo-ai_pages__error_0nkwc-j._.js"
+])
